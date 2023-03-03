b94583e210c5df39ccebd6b1b272cf018213f4fe
debabrata-97 <debabrataszr@gmail.com>
2023-03-03 12:45:04 +0530

    Compute Employee Wage for multiple companies

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,42 +1,42 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    static final int FULL_TIME = 2;
-    static final int PART_TIME = 1;
 
-    public static void main(String[] args) {
-        EmployeeWage employeeWage = new EmployeeWage();
-        employeeWage.Wage();
-    }
-        public void Wage() {
+    public static void calculateTotalWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHours) {
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        int totalWage = 0;
+        int workingHours = 0;
+        System.out.println("Details of " + companyName + " employee");
+        System.out.println("-----------------------------------------------------");
+        System.err.println("Wage per hour:" + wagePerHr);
+        System.out.println("Maximum working days:" + maxWorkingDays);
+        System.out.println("Maximum working hours:" + maxWorkingHours);
+        System.out.printf("%5s     %5s     %5s     %5s\n", "Day", "Workinghrs", "Wage", "Total working hrs");
 
-            final int WAGE_PER_HR = 20;
-            final int MAX_WORKING_DAYS = 20;
-            final int MAX_WORKING_HRS = 100;
-            int totalWage = 0;
-            int workingHours = 0;
-            for (int day = 1, totalWorkingHrs = 0; day <= MAX_WORKING_DAYS
-                    && totalWorkingHrs < MAX_WORKING_HRS; day++, totalWorkingHrs += workingHours) {
-                int empType = (int) (Math.random() * 100) % 3;
-
-                switch (empType) {
-                    case FULL_TIME:
-                        System.out.println("Employee is Present Full time");
-                        workingHours = 8;
-                        break;
-                    case PART_TIME:
-                        System.out.println("Employee is Present Part time");
-                        workingHours = 4;
-                        break;
-                    default:
-                        System.out.println("Employee is Absent");
-                        workingHours = 0;
-                        break;
-                }
-                int wage = workingHours * WAGE_PER_HR;
-                totalWage += wage;
-                System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHours, wage, totalWorkingHrs + workingHours);
+        for (int day = 1, totalWorkingHours = 0; day <= maxWorkingDays
+                && totalWorkingHours <= maxWorkingHours; day++, totalWorkingHours += workingHours) {
+            int empType = (int) (Math.random() * 100) % 3;
+            switch (empType) {
+                case FULL_TIME:
+                    workingHours = 8;
+                    break;
+                case PART_TIME:
+                    workingHours = 4;
+                    break;
+                default:
+                    workingHours = 0;
+                    break;
             }
-            System.out.println("Total wage for a month is " + totalWage);
+            int wage = workingHours * wagePerHr;
+            totalWage += wage;
+            System.out.printf("%5d       %5d      %5d      %5d\n", day, workingHours, wage, totalWorkingHours + workingHours);
         }
+        System.out.println("Total wage for a month of " + companyName + " employee is " + totalWage + "\n");
     }
+
+    public static void main(String args[]) {
+        calculateTotalWage("Accenture", 40, 15, 200);
+        calculateTotalWage("TCS", 20, 10, 100);
+    }
+}
